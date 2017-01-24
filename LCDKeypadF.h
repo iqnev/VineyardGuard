103129eb3a0afbf2733e5f875cf1b96425bc439b
fortran <qnev89@gmail.com>
2017-01-24 21:59:34 +0200

    edit contact information

--- LCDKeypadF.h
+++ LCDKeypadF.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Aug 31, 2015 Ivelin Yanev <qnev89@gmail.com>.
+ * Copyright (c) Aug 31, 2015 Ivelin Yanev <bgfortran@gmail.com>.
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
